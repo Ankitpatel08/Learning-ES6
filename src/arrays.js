7ea6aa0f24f617105425e7ee76f89680d3bd6e4a
Ankit Patel <Ankit.Patel@icrossing.com>
2017-04-07 09:38:27 +0530

    Array

--- src/arrays.js
+++ src/arrays.js
@@ -21,13 +21,38 @@ export default class arrays {
         //ES6 Array Methods
         //Array.prototype.find : iterates through array, find first element which
         //satisfy given condition in callback function
-        var number = [2, 3, 4, 5, 6];
-        var oddNumber = number.find((x) => x % 2 == 1);
+        var number = [2, 3, 4, 5, 6],
+            oddNumber = number.find((x) => x % 2 == 1);
         console.log("array oddNumber", oddNumber);
 
         //Array.prototype.findIndex : return index of element
-        var numberIndex = [5, 5, 3, 2, 5, 6];
-        var oddNumberIndex = numberIndex.findIndex((x) => x % 2 == 1);
+        var numberIndex = [5, 5, 3, 2, 5, 6],
+            oddNumberIndex = numberIndex.findIndex((x) => x % 2 == 1);
         console.log("array index of oddNumber", oddNumberIndex);
+
+        //Array.prototype.entries : returns array of arrays
+        // : child array [index, value]
+        var arrayEntries = [1, 2, 3],
+            resultArray = arrayEntries.entries();
+        console.log("array Entries - 1", resultArray.next().value);
+        console.log("array Entries - 2", [...resultArray]);
+
+        //Array.from : create array from array like values, object, Iterables : Set/Map
+        var Str1 = "Hello Ankit",
+            arrayFrom = Array.from(Str1);
+        for (let index of arrayFrom) {
+            console.log("arrayFrom", index);
+        }
+
+        //Array.from.keys() : returns the array indexes
+        var arrayKeys = Array.from(arrayFrom.keys());
+        console.log("Array.from.keys()", arrayKeys);
+
+        // destructure the array and store in individual variable
+        var arrayDestruct = ['a', 'b', 'c'],
+            [x, y, z] = arrayDestruct;
+        console.log("arrayDestruct x", x);
+        console.log("arrayDestruct y", y);
+
     }
 }
